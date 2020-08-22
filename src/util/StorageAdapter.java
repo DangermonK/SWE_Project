e0c068838219c8724ad30d8e8561f72f3941d020
hefterro <r.hefter@ltur.de>
2020-08-22 22:14:38 +0200

    added more test data.

--- src/util/StorageAdapter.java
+++ src/util/StorageAdapter.java
@@ -21,7 +21,13 @@ public class StorageAdapter {
 
     public String loadTestData() {
 
-        return "E1034;Der Schrei;Eine Menschliche Figur unter rotem Himmel, die ihre Hände gegen den Kopf presst, während sie Mund und Augen angstvoll aufreißt;Expressionismus;1910;200.0;Öl, Tempera und Pastell auf Pappe;false; . ; . ";
+        String exponatData[] = {
+                "E8593; ; ; ; ; ; ; . ; . ",
+                "E1034;Der Schrei;Eine Menschliche Figur unter rotem Himmel, die ihre Hände gegen den Kopf presst, während sie Mund und Augen angstvoll aufreißt;Expressionismus;1910;200.0;Öl, Tempera und Pastell auf Pappe;false; . ; . ",
+                "E2839;Martin-Luther;Portrait von Martin Luther;Expressionismus;1730;90000.0;Acryl;true; . ; . "
+        };
+
+        return exponatData[0];
 
 
     }
