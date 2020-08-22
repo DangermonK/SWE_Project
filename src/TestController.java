6245d5e80c1f80d39e816c3f7d63bf0f1bd05831
Julian <Julian.Mayer1996@web.de>
2020-08-22 18:53:42 +0200

    UI Detailansicht Beta

---
+++ src/TestController.java
@@ -0,0 +1,12 @@
+import de.dhbwka.swe.utils.app.SlideShowComponentApp;
+import view.GUIExponatDetails;
+
+import java.io.IOException;
+
+public class TestController {
+    public static void main(String[] args) throws IOException {
+
+        new GUIExponatDetails();
+    }
+
+}
