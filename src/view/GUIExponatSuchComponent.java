faf9ea9f3b61bf41b362d04afad963f856fa19ca
Julian <Julian.Mayer1996@web.de>
2020-08-26 21:30:25 +0200

    Last Changes

--- src/view/GUIExponatSuchComponent.java
+++ src/view/GUIExponatSuchComponent.java
@@ -1,35 +1,26 @@
 package view;
 
-import de.dhbwka.swe.utils.app.ComponentTest;
+
 import de.dhbwka.swe.utils.event.GUIEvent;
 import de.dhbwka.swe.utils.event.IGUIEventListener;
-
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class GUIExponatSuchComponent implements IGUIEventListener {
 
     JPanel suchPanel;
-    public void processGUIEvent(GUIEvent guiEvent) {
-
-    }
 
     public GUIExponatSuchComponent(){
         suchPanel = new JPanel();
         suchPanel.setLayout(new BoxLayout(suchPanel, BoxLayout.Y_AXIS));
-        JPanel rootPanel = new JPanel();
-        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
-        rootPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5,5,5,5),BorderFactory.createEtchedBorder()));
-        //suchFrame.setLayout(new BoxLayout(, BoxLayout.Y_AXIS));
+
+        suchPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5,5,5,5),BorderFactory.createEtchedBorder()));
+
         JPanel suchBar = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
         JPanel tablePane = new JPanel(new BorderLayout());
 
         JTextField suchFeld = new JTextField("Suchattribut");
-        Border blackline = BorderFactory.createLineBorder(Color.BLACK);
-        //suchFeld.setBorder(blackline);
-        //BorderFactory.createMatteBorder(5,5,5,5, Color.BLACK);
+
         suchFeld.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEtchedBorder(),(BorderFactory.createEmptyBorder(3,5,3,5))));
         JButton suchButton = new JButton("suchen");
 
@@ -40,39 +31,50 @@ public class GUIExponatSuchComponent implements IGUIEventListener {
         suchBar.add(suchBox);
         suchBar.add(suchButton);
 
-        rootPanel.add(suchBar);
+        suchPanel.add(suchBar);
 
-        String[] columnNames = {"First Name",
-                "Last Name",
-                "Sport",
-                "# of Years",
-                "Vegetarian"};
+        String[] columnNames = {"Inv-Nr.",
+                "Name",
+                "Raum",
+                "Künstler",
+                "Kategorie",
+                "Änd.-Datum"};
 
         Object[][] data = {
                 {"Kathy", "Smith",
-                        "Snowboarding", new Integer(5), new Boolean(false)},
+                        "Snowboarding", new Integer(5), new Boolean(false),"test"},
                 {"John", "Doe",
-                        "Rowing", new Integer(3), new Boolean(true)},
+                        "Rowing", new Integer(3), new Boolean(true),"test"},
                 {"Sue", "Black",
-                        "Knitting", new Integer(2), new Boolean(false)},
+                        "Knitting", new Integer(2), new Boolean(false),"test"},
                 {"Jane", "White",
-                        "Speed reading", new Integer(20), new Boolean(true)},
+                        "Speed reading", new Integer(20), new Boolean(true),"test"},
                 {"Joe", "Brown",
-                        "Pool", new Integer(10), new Boolean(false)}
+                        "Pool", new Integer(10), new Boolean(false),"test"}
         };
 
-        JTable resultTable = new JTable(data,columnNames);
+        //Deaktiviere Zelllenbearbeitung
+        JTable resultTable = new JTable(data,columnNames) {
+            public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                return false;
+            }
+        };
+
         JScrollPane scrollPane = new JScrollPane(resultTable);
 
         tablePane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         tablePane.add(scrollPane);
-        rootPanel.add(tablePane);
-        suchPanel.add(rootPanel);
+        suchPanel.add(tablePane);
+
         suchPanel.setSize(400,400);
         suchPanel.setVisible(true);
     }
 
     public JPanel getPane(){
         return  suchPanel;
+    }
+
+    public void processGUIEvent(GUIEvent guiEvent) {
+
     }
 }
